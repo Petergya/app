e76f13e61e744646015be4d1b4a26c810ed864cc
petergya <petergyang82@gmail.com>
2024-11-09 21:34:01 +0100

    fixess

--- app/src/headerFooter/Navbar.jsx
+++ app/src/headerFooter/Navbar.jsx
@@ -2,6 +2,7 @@
 "use client"
 import { useState } from 'react';
 import Link from 'next/link';
+import log from "../../public/images/Rectangle.png";
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -13,20 +14,20 @@ export default function Navbar() {
   return (
     <nav className="bg-white text-gray-800 py-4 shadow-md fixed w-full z-10" style={{ top: "20px" }}>
       <div className="container mx-auto flex items-center justify-between px-8">
-        {/* Logo */}
+        
         <div className="text-3xl font-bold py-2">
-          <Link href="/">Logo</Link>
+          <Link href="/">
+          <img src={log} alt="" />
+          </Link>
         </div>
 
-        {/* Desktop Menu Items */}
-        <div className="hidden md:flex space-x-10 ml-12">
+   
+        <div className="hidden md:flex space-x-10 mx-auto  ">
           <Link href="/welcome" className="text-lg hover:text-gray-600">Welcome to</Link>
           <Link href="/add-items" className="text-lg hover:text-gray-600">Add Items</Link>
           <Link href="/about" className="text-lg hover:text-gray-600">About</Link>
           <Link href="/contact" className="text-lg hover:text-gray-600">Contact</Link>
         </div>
-
-        {/* Mobile Menu Toggle Button */}
         <div className="md:hidden ml-auto">
           <button onClick={toggleMobileMenu} className="text-gray-800 focus:outline-none">
             <svg
@@ -46,8 +47,6 @@ export default function Navbar() {
           </button>
         </div>
       </div>
-
-      {/* Mobile Menu */}
       {isMobileMenuOpen && (
         <div className="md:hidden bg-white shadow-md">
           <div className="flex flex-col items-center space-y-4 py-4">
