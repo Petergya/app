cdc782c32e8e5cddc25db9d80d9eb41dc75dd8c7
petergya <petergyang82@gmail.com>
2024-11-09 20:30:51 +0100

    UPDATE: fixes on footer

--- app/src/headerFooter/Footer.jsx
+++ app/src/headerFooter/Footer.jsx
@@ -1,9 +1,26 @@
 
 
 
+// export default function Footer() {
+//     return (
+//         <div className="flex h-[600px] mt-16 w-screen bg-[#E82429]">
+//             <div className="h-full w-full flex items-center justify-center">
+//                 <div className="flex flex-col items-center text-center w-[80%]">
+//                     <h1 className="text-[52px] font-bold text-white">
+//                         Meet the team behind this magic
+//                     </h1>
+//                     <p className="mt-2 text-white">
+//                         We're passionate about revolutionizing food inventory management.
+//                     </p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
 export default function Footer() {
     return (
-        <div className="flex h-[600px] mt-16 w-screen bg-[#E82429]">
+        <div className="flex h-[600px] mt-16 w-screen bg-[#E82429.]">
             <div className="h-full w-full flex items-center justify-center">
                 <div className="flex flex-col items-center text-center w-[80%]">
                     <h1 className="text-[52px] font-bold text-white">
