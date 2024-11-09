44ac258eb69a0a6a21228022647c742d6c707441
petergya <petergyang82@gmail.com>
2024-11-09 15:11:16 +0100

    add hero page

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,10 +1,14 @@
 import Hero from "@/landingPageComponent/Hero";
+import OurJourney from "@/landingPageComponent/OurJourney"
+import OurTeam from "@/landingPageComponent/OurTeam";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
    <Hero />
+   <OurJourney />
+   <OurTeam />
    </>
   );
 }
--- app/src/landingPageComponent/OurTeam.jsx
+++ app/src/landingPageComponent/OurTeam.jsx
@@ -1,10 +1,46 @@
-export default function OurTeam () {
-    return(
-        <div className="text center">
-            <h1 className="text-center text-3xl font-bold text-red-800">
-            Our Team
-            </h1>
-           
-        </div>
-    )
+
+
+
+import Image from "next/image";
+
+export default function OurTeam() {
+  const images = [
+    "/images/Rectangle 42.png",
+    "/images/Rectangle 43.png",
+    "/images/Rectangle 44.png",
+  ];
+
+  return (
+    <div>
+      
+      <div className="text-center mt-24">
+        <h1 className="text-[42px] font-bold">Our Team</h1>
+        <p className="text-[24px] mt-2">
+          Meet the dedicated individuals behind our innovative food inventory solution
+        </p>
+      </div>
+
+      
+      <div className="relative w-[324px] h-[317px] mx-auto mt-32 overflow-hidden rounded-xl">
+        <Image
+          src={images[0]}
+          alt="A beautiful example image"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      
+      <div className="flex justify-between mx-28 mt-16">
+        {images.slice(1).map((image, index) => (
+          <div
+            key={index}
+            className="relative w-[324px] h-[317px] overflow-hidden rounded-xl"
+          >
+            <Image src={image} alt={`Image ${index + 2}`} layout="fill" objectFit="cover" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
